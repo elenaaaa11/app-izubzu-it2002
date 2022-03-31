aa2d6ada86327987a78745b4e23918ed3279597b
unknown <e0376959@u.nus.edu>
2022-04-01 04:42:33 +0800

    post house

--- AppStore/urls.py
+++ AppStore/urls.py
@@ -7,9 +7,8 @@ urlpatterns = [
     path('', app.views.index, name='index'),
     path('rent', app.views.rent, name='rent'),
     path('register', app.views.register, name='register'),
-    #path('post', app.views.post, name='post'),
+    path('post', app.views.post, name='post'),
     path('login', app.views.login, name='login'),
     path('admin/', admin.site.urls),
     path('view/<str:title>', app.views.view, name='view')
-    path('post', app.views.post, name='post')
 ]
