f6298b11838946608cfcc9607259c30faa4c7eed
elenaaaa11 <99041871+elenaaaa11@users.noreply.github.com>
2022-04-04 21:53:51 +0800

    Update urls.py

--- AppStore/urls.py
+++ AppStore/urls.py
@@ -3,15 +3,20 @@ from django.urls import path
 
 import app.views
 
+
 urlpatterns = [
     path('', app.views.index, name='index'),
     path('rent', app.views.rent, name='rent'),
+    path('area/<str:area_name>', app.views.area, name='area'),
     path('register', app.views.register, name='register'),
     path('post', app.views.post, name='post'),
     path('login', app.views.user_login, name='login'),
-    path('logout', app.views.user_logout, name='logout'),
     path('admin/', admin.site.urls),
     path('view/<str:title>', app.views.view, name='view'),
     path('rent_1/<str:title>', app.views.rent_1, name='rent_1'),
-    path('view/rent_1/<str:title>', app.views.rent_1, name='rent_2')
+    path('view/rent_1/<str:title>', app.views.rent_1, name='rent_2'),
+    path('area/view/<str:title>', app.views.view, name='view_area'),
+    path('area/rent_1/<str:title>', app.views.rent_1, name='rent_area'),
+    path('area/view/rent_1/<str:title>', app.views.rent_1, name='rent_area_1')
+    
 ]
